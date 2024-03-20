b70687e5158159645a9814566cb61bb1c8d0c7bc
popori <ujk6073@gmail.com>
2024-03-20 18:03:05 +0900

    :tada: start garabu!!

---
+++ src/main/java/garabu/garabuServer/GarabuServerApplication.java
@@ -0,0 +1,13 @@
+package garabu.garabuServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarabuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarabuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garabu/garabuServer/GarabuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package garabu.garabuServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarabuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
