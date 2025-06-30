24420b1e342d8bc658a8a1834153a87da8b77c4f
yhj0904 <ujk6073@gmail.com>
2025-06-30 13:38:27 +0900

    :recycle: login pk modify ...

--- src/main/java/garabu/garabuServer/dto/UserDTO.java
+++ src/main/java/garabu/garabuServer/dto/UserDTO.java
@@ -10,4 +10,5 @@ public class UserDTO {
     private String name;
     private String username;
     private String email;
+    private String providerId;
 }
--- src/main/java/garabu/garabuServer/repository/MemberJPARepository.java
+++ src/main/java/garabu/garabuServer/repository/MemberJPARepository.java
@@ -4,10 +4,13 @@ import garabu.garabuServer.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberJPARepository extends JpaRepository<Member, Long> {
     List<Member> findByEmail(String email);
     Member findByUsername(String username);
     Member findByName(String name);
     Member findOneByEmail(String email);
+    Optional<Member> findByProviderIdAndEmail(String providerId, String email);
+
 }
